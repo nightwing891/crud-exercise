f59cb02300258fa98170ca2b484da9a3ce5f8c8f
Daniel Treasure <dbtreasure@gmail.com>
2015-06-29 16:42:19 -0600

    creates html elements and basic controller functions to handle crud operations on json server db

--- app.js
+++ app.js
@@ -4,10 +4,34 @@ app.service('articles', ['$http', function ($http) {
 	this.getArticles = function(){
 		return $http.get('http://localhost:3000/posts')
 	}
+
+	//this.deleteArticle = function...
+
+	//this.updateArticle = function...
+
+	//this.createArticle = function...
 }])
 
 app.controller('myCtrl', ['$scope', 'articles', function ($scope, articles) {
 	articles.getArticles().success(function(data){
 		$scope.articles = data;
-	})
+	});
+
+	$scope.updatedArticle = {};
+
+	$scope.newArticle = {};
+
+	$scope.delete = function(id){
+		console.log(id)
+	};
+
+	$scope.update = function(id){
+
+		$scope.updateArticle = {};
+	}
+
+	$scope.create = function(){
+
+		$scope.newArticle = {};
+	}
 }])
